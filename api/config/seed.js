3d1ed258fd942a1b189a1542bbd81ca9bcc9c60d
Samar Yalini <samarjben@outlook.com>
2021-10-11 22:36:10 +0530

    api: bugfix: seed

--- api/config/seed.js
+++ api/config/seed.js
@@ -3,19 +3,25 @@ require('dotenv').config()
 require('./db')
 const {User, FactType} = require("../app/models");
 
-const user = new User()
-user.name = "Admin"
-user.email = "admin@yalusoft.com"
-user.password = "123456"
-user.role = "admin"
-user.save().then()
+const email = "admin@yalusoft.com";
+
+User.findOne({email}).exec().then(u => {
+    if (u) return;
+
+    const user = new User()
+    user.name = "Admin"
+    user.email = email
+    user.password = "123456"
+    user.role = "admin"
+    user.save().then()
+}).catch(e => console.log("User Insertion Failed", e.message))
 
 
 const factTypes = ['Fact', 'Fun Fact', 'Did You Know', 'Psychology Fact']
-factTypes.forEach(v => {
-    FactType.findOne({ name: 'Fact' }).exec().then(v => {
+factTypes.forEach(name => {
+    FactType.findOne({ name }).exec().then(v => {
         if (!v) {
-            const f = new FactType({name: v})
+            const f = new FactType({name})
             f.save().then()
         }
     })
